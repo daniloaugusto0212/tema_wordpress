a00cee28e5009c2bcf7150d71fd61e74023f4d50
Danilo <daniloaugusto0212@hotmail.com>
2021-09-04 15:26:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Tema Wordpress</title>
+</head>
+<body>
+    
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="assets/img/home/pngs/logo.png" alt="Logomarca"></div>
+                <!-- /.logo -->
+            </header>
+            <div class="menu-desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <!-- /.menu-desktop -->
+            <div class="clear"></div>
+        </div>
+        <!-- /.center -->
+    </section>
+    <!-- /.topo -->
+</body>
+</html>
