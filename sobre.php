23a558a9caee38dd7e35289b563c7fb2010338c7
Danilo <daniloaugusto0212@hotmail.com>
2021-09-07 15:54:53 -0300

    Finalizando página sobre

--- sobre.php
+++ sobre.php
@@ -63,7 +63,34 @@
            <div class="clear"></div>
        </div>
        <!-- /.center -->
+    </section>
     <!-- /.sobre-equipe -->
+
+    <section class="galeria">
+        <div class="center">
+            <div class="box-imagens">
+                <div class="box-imagem-single">
+                    <img src="img/about/foto-1.png" alt="">
+                </div>
+                <!-- /.box-imagem-single -->
+                <div class="box-imagem-single">
+                    <img src="img/about/foto-2.png" alt="">
+                </div>
+                <!-- /.box-imagem-single -->
+                <div class="box-imagem-single">
+                    <img src="img/about/foto-3.png" alt="">
+                </div>
+                <!-- /.box-imagem-single -->
+                <div class="box-imagem-single">
+                    <img src="img/about/foto-4.png" alt="">
+                </div>
+                <!-- /.box-imagem-single -->
+            </div>
+            <!-- /.box-imagens -->
+        </div>
+        <!-- /.center -->
+    </section>
+    <!-- /.galeria -->
 
     <footer>
         <div class="metodologia">
