7d6eb7c21a0a7ac3eea57c6d4ff31731bc21ba54
Danilo <daniloaugusto0212@hotmail.com>
2021-09-07 15:35:37 -0300

    Iniciando página sobre

---
+++ sobre.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Tema Wordpress</title>
+</head>
+<body>
+    
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="img/home/pngs/logo.png" alt="Logomarca"></div>
+                <!-- /.logo -->
+            </header>
+            <div class="menu-desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <!-- /.menu-desktop -->
+
+            <div class="menu-mobile">
+                <i class="fas fa-bars"></i>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <!-- /.menu-mobile -->
+            <div class="clear"></div>            
+        </div>
+        <!-- /.center -->
+    </section>
+    <!-- /.topo -->
+
+    <section class="sobre-equipe">
+       <div class="center">
+           <div class="w50">
+               <div class="sobre-equipe-descricao">
+                   <h2>Uma equipe estratégica para um trabalho incrível</h2>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe repudiandae tempore molestias, mollitia aperiam omnis voluptates ex ut cumque velit sapiente accusantium corporis expedita! Recusandae dolorum dolorem totam voluptate?</p>
+               </div>
+               <!-- /.sobre-equipe-descricao -->
+           </div>
+           <!-- /.w50 -->
+           <div class="w50">
+               <div class="sobre-equipe-imagem">
+                   <img src="img/about/foto-equipe-01.png" alt="">
+                </div>
+           </div>
+           <!-- /.w50 -->
+           <div class="clear"></div>
+       </div>
+       <!-- /.center -->
+    <!-- /.sobre-equipe -->
+
+    <footer>
+        <div class="metodologia">
+            <div class="center">
+                <h2>Conheça nossa metodologia</h2>
+                <p>O que acha de fazermos o que mais gostamos de fazer? Conversar! <br>Entre em contato por e-mail ou telefone.</p>
+                <a href="">Entrar em contato</a>
+            </div>
+            <!-- /.center -->
+        </div>
+        <!-- /.metodologia -->
+        <div class="center">
+            <div class="col-footer">
+                <h2>Suporte</h2>
+                <a href="">Contato</a>
+                <a href="">Faq</a>
+            </div>
+            <!-- /.col-footer -->
+            <div class="col-footer">
+                <h2>Consultoria</h2>
+                <a href="">Contato</a>
+                <a href="">Faq</a>
+            </div>
+            <!-- /.col-footer -->
+            <div class="col-footer">
+                <h2>Empresa</h2>
+                <a href="">Contato</a>
+                <a href="">Faq</a>
+            </div>
+            <!-- /.col-footer -->
+            <div style="width: 40%; text-align:right;" class="col-footer">
+                <img src="img/home/pngs/logo-dummy.png" alt="">
+            </div>
+            <!-- /.col-footer -->
+        </div>
+        <!-- /.center -->
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+    <script src="scripts.js"></script>
+</body>
+</html>
